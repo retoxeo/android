8d000b6b5fb304f9bdd40a2be0cc8e0ca224200f
XEO <retoxeo8@gmail.com>
2024-10-28 08:24:09 +0100

    Initial commit

---
+++ app/src/test/java/com/dam2/reto_1_xeo/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.dam2.reto_1_xeo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
