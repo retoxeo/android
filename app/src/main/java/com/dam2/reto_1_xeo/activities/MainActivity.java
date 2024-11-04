5d2a1805d66ddb2268cf42a32eb6552b0110d95c
XEO <retoxeo8@gmail.com>
2024-11-04 09:26:34 +0100

    Popup menu

--- app/src/main/java/com/dam2/reto_1_xeo/activities/MainActivity.java
+++ app/src/main/java/com/dam2/reto_1_xeo/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package com.dam2.reto_1_xeo.activities;
 
 import android.os.Bundle;
-
-import com.dam2.reto_1_xeo.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,7 +13,9 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.dam2.reto_1_xeo.R;
 import com.dam2.reto_1_xeo.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Objects;
 
@@ -33,5 +37,36 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        ImageView profileIcon = findViewById(R.id.profile);
+        profileIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showProfileMenu(view);
+            }
+        });
+    }
+
+    private void showProfileMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.getMenuInflater().inflate(R.menu.profile_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.action_login) {
+                    Toast.makeText(MainActivity.this, "Login seleccionado", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (item.getItemId() == R.id.action_profile) {
+                    Toast.makeText(MainActivity.this, "Perfil seleccionado", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (item.getItemId() == R.id.action_logout) {
+                    Toast.makeText(MainActivity.this, "Cerrar sesión seleccionado", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
     }
 }
