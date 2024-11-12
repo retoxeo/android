74892b4f7596b371647ae784ba9321da6f3edf84
Eneko <enekorg22@gmail.com>
2024-11-12 21:37:21 +0100

    Perfil

--- app/src/main/java/com/dam2/reto_1_xeo/fragments/ProfileFragment.java
+++ app/src/main/java/com/dam2/reto_1_xeo/fragments/ProfileFragment.java
@@ -1,15 +1,24 @@
 package com.dam2.reto_1_xeo.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.bumptech.glide.Glide;
 import com.dam2.reto_1_xeo.R;
 import com.dam2.reto_1_xeo.activities.MainActivity;
+import com.dam2.reto_1_xeo.models.LoginResponse.UserData;
+import com.dam2.reto_1_xeo.utils.SharedPreferencesHelper;
 
 public class ProfileFragment extends Fragment {
 
@@ -25,5 +34,31 @@ public class ProfileFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView usernameTextView = view.findViewById(R.id.usernameTextView);
+        TextView emailTextView = view.findViewById(R.id.emailTextView);
+        TextView addressTextView = view.findViewById(R.id.addressTextView);
+        ImageView profileImageView = view.findViewById(R.id.profileImageView);
+
+        UserData userData = SharedPreferencesHelper.getUserData(requireActivity());
+
+        if (userData != null) {
+            usernameTextView.setText(userData.getNombre() + " " + userData.getApellido1() + " " + userData.getApellido2());
+            emailTextView.setText(userData.getCorreo());
+
+            String address = userData.getCalle() + " " + userData.getNumero();
+            addressTextView.setText(address);
+
+            Glide.with(this)
+                    .load(userData.getFoto())
+                    .error(R.drawable.obras)
+                    .into(profileImageView);
+        }
     }
 }
--- app/src/main/java/com/dam2/reto_1_xeo/utils/SharedPreferencesHelper.java
+++ app/src/main/java/com/dam2/reto_1_xeo/utils/SharedPreferencesHelper.java
@@ -18,10 +18,14 @@ public class SharedPreferencesHelper {
     private static final String KEY_USER_CALLE = "user_calle";
     private static final String KEY_USER_NUMERO = "user_numero";
     private static final String KEY_USER_CP = "user_cp";
+    private static final String KEY_USER_FOTO = "user_foto";
+    private static final String KEY_USER_APELLIDO1 = "user_apellido1";
+    private static final String KEY_USER_APELLIDO2 = "user_apellido2";
 
     public static void saveUserData(Context context, UserData userData) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
+
         editor.putInt(KEY_USER_ID, userData.getId());
         editor.putString(KEY_USER_NAME, userData.getNombre());
         editor.putString(KEY_USER_EMAIL, userData.getCorreo());
@@ -32,37 +36,49 @@ public class SharedPreferencesHelper {
         editor.putString(KEY_USER_CALLE, userData.getCalle());
         editor.putInt(KEY_USER_NUMERO, userData.getNumero());
         editor.putInt(KEY_USER_CP, userData.getCp());
+        editor.putString(KEY_USER_FOTO, userData.getFoto());
+        editor.putString(KEY_USER_APELLIDO1, userData.getApellido1());
+        editor.putString(KEY_USER_APELLIDO2, userData.getApellido2());
+
         editor.apply();
     }
 
     public static UserData getUserData(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
         int id = sharedPreferences.getInt(KEY_USER_ID, -1);
-        String name = sharedPreferences.getString(KEY_USER_NAME, null);
-        String email = sharedPreferences.getString(KEY_USER_EMAIL, null);
-        String phone = sharedPreferences.getString(KEY_USER_PHONE, null);
-        String country = sharedPreferences.getString(KEY_USER_COUNTRY, null);
-        String city = sharedPreferences.getString(KEY_USER_CITY, null);
-        String province = sharedPreferences.getString(KEY_USER_PROVINCE, null);
+        String nombre = sharedPreferences.getString(KEY_USER_NAME, null);
+        String correo = sharedPreferences.getString(KEY_USER_EMAIL, null);
+        String telefono = sharedPreferences.getString(KEY_USER_PHONE, null);
+        String pais = sharedPreferences.getString(KEY_USER_COUNTRY, null);
+        String ciudad = sharedPreferences.getString(KEY_USER_CITY, null);
+        String provincia = sharedPreferences.getString(KEY_USER_PROVINCE, null);
         String calle = sharedPreferences.getString(KEY_USER_CALLE, null);
         int numero = sharedPreferences.getInt(KEY_USER_NUMERO, -1);
         int cp = sharedPreferences.getInt(KEY_USER_CP, -1);
+        String foto = sharedPreferences.getString(KEY_USER_FOTO, null);
+        String apellido1 = sharedPreferences.getString(KEY_USER_APELLIDO1, null);
+        String apellido2 = sharedPreferences.getString(KEY_USER_APELLIDO2, null);
 
-        if (id == -1 || name == null || email == null) {
+        if (id == -1 || nombre == null || correo == null) {
             return null;
         }
 
         UserData userData = new UserData();
         userData.setId(id);
-        userData.setNombre(name);
-        userData.setCorreo(email);
-        userData.setTelefono(phone);
-        userData.setPais(country);
-        userData.setCiudad(city);
-        userData.setProvincia(province);
+        userData.setNombre(nombre);
+        userData.setCorreo(correo);
+        userData.setTelefono(telefono);
+        userData.setPais(pais);
+        userData.setCiudad(ciudad);
+        userData.setProvincia(provincia);
         userData.setCalle(calle);
         userData.setNumero(numero);
         userData.setCp(cp);
+        userData.setFoto(foto);
+        userData.setApellido1(apellido1);
+        userData.setApellido2(apellido2);
+
         return userData;
     }
 
